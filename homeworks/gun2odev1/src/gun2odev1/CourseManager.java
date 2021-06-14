967589312666c402847a2bb337086c4baa3184c3
Hilalbiyik <hilalbyk396@gmail.com>
2021-06-14 10:08:00 +0300

    gun2odev1

---
+++ homeworks/gun2odev1/src/gun2odev1/CourseManager.java
@@ -0,0 +1,9 @@
+package gun2odev1;
+
+public class CourseManager {
+	
+	public void addCourse(Course course) {
+		System.out.println(course.title + " Kursu sisteme eklenmiştir.");
+	}
+
+}
---
+++ homeworks/gun2odev1/src/gun2odev1/StudentManager.java
@@ -0,0 +1,9 @@
+package gun2odev1;
+
+public class StudentManager {
+	
+		public void addStudent (Student student) {
+		System.out.println(student.studentName +" "+ student.studentSurname  +" Öğrenci kaydı başarıyla yapıldı ");
+
+}
+}
