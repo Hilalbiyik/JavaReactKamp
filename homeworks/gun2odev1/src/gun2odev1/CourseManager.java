b7d7dd58c18dbdcc3bcc02c192656a05159c3e4e
Hilal Bıyık <77548014+Hilalbiyik@users.noreply.github.com>
2021-06-14 10:02:06 +0300

    Delete homeworks/gun2odev1 directory

--- homeworks/gun2odev1/src/gun2odev1/CourseManager.java
+++
@@ -1,8 +0,0 @@
-package gun2odev1;
-
-public class CourseManager {
-	public void addCourse(Course course) {
-		System.out.println(course.title + " Kursu sisteme eklenmiştir.");
-	}
-
-}
--- homeworks/gun2odev1/src/gun2odev1/StudentManager.java
+++
@@ -1,8 +0,0 @@
-package gun2odev1;
-
-public class StudentManager {
-		public void addStudent (Student student) {
-		System.out.println(student.studentName +" "+ student.studentSurname  +" Öğrenci kaydı başarıyla yapıldı ");
-
-}
-}
